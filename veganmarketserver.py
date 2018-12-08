94a826f4f2434bea9eaa98278e7e229ddc63f7f0
atheer <atheerabdullaha@outlook.com>
2018-12-08 14:33:23 +0300

    fix log in 1

--- veganmarketserver.py
+++ veganmarketserver.py
@@ -117,6 +117,38 @@ def gconnect():
     print "done!"
     return output
 
+@app.route('/gdisconnect')
+def gdisconnect():
+    access_token = login_session.get('access_token')
+    if access_token is None:
+        print 'Access Token is None'
+        response = make_response(json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    print 'In gdisconnect access token is %s', access_token
+    print 'User name is: '
+    print login_session['username']
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    print 'result is '
+    print result
+    if result['status'] == '200':
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        response = make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    else:
+        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+
+
 
 # JSON APIs to view a department Information
 @app.route('/departments/<int:department_id>/items/JSON')
@@ -150,7 +182,8 @@ def showDepartments():
 # Create a new department
 @app.route('/departments/new/', methods=['GET', 'POST'])
 def newDepartment():
-
+    if 'username' not in login_session:
+        return redirect('/login')
     if request.method == 'POST':
         newDepartment = Department(name=request.form['name'], image=request.form['image'])
         try:
